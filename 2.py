a8e2ff3096f795553a0c22d1b568167d86433351
theekshana <theekshana.samaradiwakara@madmobile.com>
2022-06-30 23:50:26 +0530

    first commit

---
+++ 2.py
@@ -0,0 +1,10 @@
+def show_excitement():
+    # Your code goes here!
+    string = "I am super excited for this course!"
+    concat = ""
+    for i in range(5):
+        concat += string + " "
+    return concat
+
+
+print (show_excitement())
